72c01c6ddf15ad86667c96c85cbce43240986c0d
anandalagappa <61884176+anandalagappa@users.noreply.github.com>
2021-05-02 13:59:23 -0400

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,3 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-<<<<<<< HEAD
-print "This is a Spatial Web " . PHP_VERSION . "\n";
-=======
-print "A change is in the air!\n";
->>>>>>> faee96a2209202cb92c6f9b46cb0089f1b8f5f4c
 ?>
